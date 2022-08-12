2fcf474f215491bf02e9604dbdecebda976534ac
Martín Calderón <elmcwl@gmail.com>
2022-08-11 22:19:03 -0600

    header

--- src/vistas/headerHome.php
+++ src/vistas/headerHome.php
@@ -12,7 +12,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-
+    
+    <!-- Bootstrap iconos font -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -27,17 +29,21 @@
         </a>
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li>
-                    <a href="#" nav-link px-2 link-secondary>Aspirantes</a>
+                    <a href="#" class="nav-link px-4 link-secondary">Aspirantes</a>
                 </li>
                 <li>
-                    <a href="#" nav-link px-2 link-secondary>Estudiantes</a>
+                    <a href="#" class="nav-link px-4 link-secondary">Estudiantes</a>
                 </li>
                 <li>
-                    <a href="#" nav-link px-2 link-secondary>Egreso</a>
+                    <a href="#" class="nav-link px-4 link-secondary">Egreso</a>
                 </li>
                 <li>
-                    <a href="#" nav-link px-2 link-secondary>Docentes</a>
+                    <a href="#" class="nav-link px-4 link-secondary">Docentes</a>
                 </li>
             </ul>
+            <div class="col-md-3 text-end">
+                <i class="bi bi-search"></i>
+                <i class="bi bi-facebook"></i>
+            </div>
 </header>
     </div>
