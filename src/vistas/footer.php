6a6c3ec8fc15a176cfcdfeece2a6873ca87ca28c
Juan Morales <juanmorales@iMac-de-Juan.local>
2022-08-17 16:41:50 -0600

    Footer changes

--- src/vistas/footer.php
+++ src/vistas/footer.php
@@ -1,3 +1,25 @@
-<footer>
-    <h1>hola este es el footer</h1>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h2>Enlaces de innterés</h2>
+                <ul>
+                    <li><a href="#">Oferta Educativa</a></li>
+                    <li><a href="#">Responsabilidad Social Universitaria</a></li>
+                    <li><a href="#">Transparencia</a></li>
+                    <li><a href="#">Contraloría Social</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h2>Nuestra Universidad</h2>
+                <ul>
+                    <li><a href="#">Curriculum Institucional</a></li>
+                    <li><a href="#">Órganos de Gobierno</a></li>
+                    <li><a href="#">Normatividad</a></li>
+                    <li><a href="#">Mapa del Campus</a></li>
+                    <li><a href="#">Calendario Escolar</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
--- src/vistas/header.php
+++ src/vistas/header.php
@@ -15,6 +15,7 @@
     
     <!-- Bootstrap iconos font -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 
 <body>
