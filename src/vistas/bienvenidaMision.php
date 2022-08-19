8ed1004c1e54eefda8209a60f0ec3090927077b9
Martín Calderón <elmcwl@gmail.com>
2022-08-19 13:05:31 -0600

    mision-vision

---
+++ src/vistas/bienvenidaMision.php
@@ -0,0 +1,55 @@
+<div class="container-sm">
+    <div class="">
+        <div class="bienvenida-titulo">
+            <h2 class="">Mensaje de Bienvenida</h2>
+            <div class="v-line"></div>
+            <h2 class=" bienvenida-gris">Welcome Mesagge</h2>
+        </div>
+        <div class="bienvenida-carta">
+            <p>
+                Deseo manifestarles gran alegría y satisfacción al brindarles una cordial y afectuosa
+                bienvenida a la
+                Universidad Autónoma de Baja California Sur y muy especialmente al Departamento de Sistemas
+                Computacionales.
+                Nuestra alma mater, institución educativa que ha sido distinguida a nivel estatal, regional, nacional e
+                internacional por su calidad académica, artística y deportiva.
+            </p>
+            <p>
+                Jóvenes, ahora universitarios, han dado un trascendental paso en sus anhelos por lograr una excelente
+                formación profesional y personal, por ello los felicito y los exhorto a iniciar y a continuar con
+                alegría,
+                responsabilidad y con muchas ganas sus estudios en la UABCS, para que aporten lo mejor de sus
+                conocimientos
+                y habilidades en su futuro desempeño como profesionistas. </p>
+        </div>
+    </div>
+
+    <div class="bienvenida-cita">
+        <div class="v-line"></div>
+        <p>Dr. Jesús Andrés Sandoval Bringas
+            Jefe del Departamento Académico de Sistemas Computacionales</p>
+    </div>
+
+</div>
+
+
+<div class="mision-vision bg-primary d-flex" style="height: 597px; ">
+    <!-- <img src="img/mision.png" alt="" style="width: 100%;"> -->
+    <div class="container-sm align-self-center">
+        <div class="mision-titulo">
+            <h4>MISIÓN</h3>
+                <div class="v-line"></div>
+                <h4>VISIÓN</h3>
+        </div>
+        <p>
+            Formar recursos humanos profesionales, innovadores y participativos, con un alto sentido ético y con
+            capacidad crítica, de análisis, de investigación y dirección, que con responsabilidad, honestidad y
+            solidaridad aporten soluciones mediante el diseño, desarrollo y aplicación de tecnologías
+            computacionales y
+            difundan la cultura con espíritu emprendedor, comprometidos con el desarrollo integral sustentable
+            de su
+            entorno.
+        </p>
+    </div>
+
+</div>
--- src/vistas/header.php
+++ src/vistas/header.php
@@ -9,50 +9,75 @@
 
     <!-- bootstrap 5.2-->
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+
     <!-- Bootstrap iconos font -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <!--Estilos locales-->
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/oferta.css">
+    <link rel="stylesheet" href="css/header.css">
     <!--slider cdn-->
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 </head>
 
 <body>
-    
-    <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
-        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <div>
+        <header
+            class="container-fluid | md:font-light md:text-sm flex flex-row  justify-between bg-transparent relative d-flex flex-wrap align-items-center header">
 
-        <img src="https://www.uabcs.mx/assets/images/tail-assets/burguer-b.png" style="color: black;" alt="UABCS" class="p-2">
+            <a href="/" class=" col-md-2">
+                <img src="https://www.uabcs.mx/assets/images/tail-assets/burguer-b.png" style="color: black;"
+                    alt="UABCS" class="p-2">
+            </a>
 
-        </a>
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <ul class="nav align-items-center">
                 <li>
-                    <a href="#" class="nav-link px-4 link-secondary">Aspirantes</a>
+                    <a href="#" class="nav-link px-4 link-secondary text-dark">Aspirantes <i
+                            class="bi bi-caret-down-fill"></i></a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link px-4 link-secondary">Estudiantes</a>
+                    <a href="#" class="nav-link px-4 link-secondary text-dark">Estudiantes <i
+                            class="bi bi-caret-down-fill"></i></a>
+                </li>
+
+                <li>
+                    <a href="#" class="px-4 link-secondary"><img src="img/dasc-logo.png" alt="dasc-logo"
+                            style="height: 54px;"></a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link px-4 link-secondary">Egreso</a>
+                    <a href="#" class="nav-link px-4 link-secondary text-dark">Egreso <i
+                            class="bi bi-caret-down-fill"></i></a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link px-4 link-secondary">Docentes</a>
+                    <a href="#" class="nav-link px-4 link-secondary text-dark">Docentes <i
+                            class="bi bi-caret-down-fill"></i></a>
                 </li>
             </ul>
-            <div class="col-md-3 text-end">
+            <div class="col-md-2 text-end">
                 <i class="bi bi-search"></i>
                 <i class="bi bi-facebook"></i>
+
             </div>
-</header>
+        </header>
+    </div>
+
+    
+    <div>
+        <video autoplay="autoplay" loop="loop" id="video_background" preload="auto" muted="" class="banner-video container-fluid" style="padding: 0px;">
+            <source src="https://www.uabcs.mx/assets/departments/dah/dah.mp4" type="video/mp4">
+        </video>
+    </div>
+
+
+    <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     </div>
